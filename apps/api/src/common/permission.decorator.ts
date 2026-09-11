b208dff41f54f4dcaa48552d85ba146f39fe4925
alexoaraujo83 <alexoaraujo83@gmail.com>
2026-09-11 20:28:09 -0300

    feat(api): add permission metadata decorator

---
+++ apps/api/src/common/permission.decorator.ts
@@ -0,0 +1,5 @@
+import { SetMetadata } from '@nestjs/common';
+import { REQUIRED_PERMISSION } from './permission.guard.js';
+
+export const RequirePermission = (permission: string): MethodDecorator & ClassDecorator =>
+  SetMetadata(REQUIRED_PERMISSION, permission);
