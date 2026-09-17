e220fd9890b13c3be4ae7a6c6ec37ff07f33969a
alexoaraujo83 <alexoaraujo83@gmail.com>
2026-09-17 19:04:20 -0300

    test(security): add minimal Node test type shim

---
+++ packages/security/src/node-test-shim.d.ts
@@ -0,0 +1,12 @@
+declare module "node:test" {
+  const test: (...args: unknown[]) => unknown;
+  export default test;
+}
+
+declare module "node:assert/strict" {
+  const assert: {
+    equal: (...args: unknown[]) => unknown;
+    throws: (...args: unknown[]) => unknown;
+  };
+  export default assert;
+}
