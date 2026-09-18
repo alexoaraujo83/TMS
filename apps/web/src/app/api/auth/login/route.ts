af1d8f41e364b6d6bfd82c8920ebd5486ca46022
alexoaraujo83 <alexoaraujo83@gmail.com>
2026-09-18 00:35:22 -0300

    feat(web): add Auth0 OIDC session foundation

---
+++ apps/web/src/app/api/auth/login/route.ts
@@ -0,0 +1,6 @@
+import { NextResponse } from "next/server";
+import { buildLoginUrl } from "../../../../../../lib/auth0";
+
+export async function GET() {
+  return NextResponse.redirect(await buildLoginUrl());
+}
