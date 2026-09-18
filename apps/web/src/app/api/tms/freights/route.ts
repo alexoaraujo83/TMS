47729b67776733814f81be18c2e2404798eefb2b
alexoaraujo83 <alexoaraujo83@gmail.com>
2026-09-18 00:35:53 -0300

    fix(web): correct auth route imports

--- apps/web/src/app/api/tms/freights/route.ts
+++ apps/web/src/app/api/tms/freights/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { getAccessToken } from "../../../../../lib/auth0";
+import { getAccessToken } from "../../../../lib/auth0";
 
 export async function GET() {
   const token = await getAccessToken();
