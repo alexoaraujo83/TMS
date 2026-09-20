c68c19835cb99db7a03fc1ea5db90fde0ebd737e
alexoaraujo83 <alexoaraujo83@gmail.com>
2026-09-19 21:06:17 -0300

    refactor(web): remove manual Auth0 flow

--- apps/web/src/app/api/auth/login/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-import { buildLoginUrl } from "../../../../lib/auth0";
-
-export async function GET() {
-  return NextResponse.redirect(await buildLoginUrl());
-}
