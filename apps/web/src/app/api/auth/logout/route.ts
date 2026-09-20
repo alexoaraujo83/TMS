d6abd92ca4b3e242259c4e72124870850a2cbbdb
alexoaraujo83 <alexoaraujo83@gmail.com>
2026-09-19 21:31:21 -0300

    refactor(web): remove manual Auth0 route handler

--- apps/web/src/app/api/auth/logout/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { clearSession, logoutUrl } from "../../../../lib/auth0";
-
-export async function GET() {
-  await clearSession();
-  return NextResponse.redirect(logoutUrl());
-}
