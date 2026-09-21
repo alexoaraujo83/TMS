b5f1e52ba449adef27fbb70b0aa92a1bbf305fa1
alexoaraujo83 <alexoaraujo83@gmail.com>
2026-09-21 11:23:14 -0300

    refactor: remove unused shared package

--- packages/shared/src/index.test.ts
+++
@@ -1,8 +0,0 @@
-import test from "node:test";
-import assert from "node:assert/strict";
-
-import { APP_NAME } from "./index.js";
-
-test("exports the canonical application name", () => {
-  assert.equal(APP_NAME, "tms");
-});
